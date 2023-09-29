57c204509930453b07e9788a88d8fbc8f84a05c3
Michał Masełko <michalmaselko@wp.pl>
2023-09-29 20:53:47 +0200

    change URL adress in downloader

--- src/main/java/Downloader/Downloader.java
+++ src/main/java/Downloader/Downloader.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class Downloader {
     public static void main(String[] args) throws IOException {
-        Document document = Jsoup.connect("https://www.wykop.pl").get();
-        Elements h2Elements = document.select("h2");
+        Document document = Jsoup.connect("https://www.machineseeker.com/Milling-machines/ci-218").get();
+        Elements h2Elements = document.select("h5");
         for (Element h2: h2Elements) {
             String h2Text = h2.text();
             System.out.println(h2Text);
